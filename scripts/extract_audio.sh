a4ed2bd2812c0f84f84aa7deb5ad35125b0aa6ec
knmac <khoinguyenx@gmail.com>
2020-06-09 11:15:44 -0500

    Add preprocessing tools for audio

---
+++ scripts/extract_audio.sh
@@ -0,0 +1,3 @@
+python tools/preprocessing_epic/extract_audio.py \
+    --videos_dir "data/EPIC_KITCHENS_2018/videos" \
+    --output_dir "data/EPIC_KITCHENS_2018/audio"
---
+++ scripts/wav_to_dict.sh
@@ -0,0 +1,3 @@
+python tools/preprocessing_epic/wav_to_dict.py \
+    --sound_dir "data/EPIC_KITCHENS_2018/audio" \
+    --output_dir "data/EPIC_KITCHENS_2018/audio_dict.pkl"
