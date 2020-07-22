a94d9b200f259f1b576b34e253bb6f3740de2735
knmac <khoinguyenx@gmail.com>
2020-07-22 13:37:39 -0500

    change experiment name

--- scripts/run_pipeline_rgbspec_san19pairfreeze_avghallu.sh
+++ scripts/run_pipeline_rgbspec_san19pairfreeze_avghallu.sh
@@ -3,7 +3,7 @@ PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         'configs/model_cfgs/pipeline_rgbspec_san19pairfreeze_avghallu.yaml' \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens.yaml' \
     --train_cfg         'configs/train_cfgs/train_san_freeze.yaml' \
-    --experiment_suffix 'san19pairfreeze_gruhallu' \
+    --experiment_suffix 'san19pairfreeze_avghallu' \
     --is_training       true \
     --train_mode        'from_scratch' \
     --logdir            'logs' \
