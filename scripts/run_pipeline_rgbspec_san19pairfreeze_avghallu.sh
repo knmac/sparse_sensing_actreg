27abe5255d1813f021793f28efbd82d04295d52f
knmac <khoinguyenx@gmail.com>
2020-07-28 17:05:31 -0500

    change default training setting and use SSIM loss

--- scripts/run_pipeline_rgbspec_san19pairfreeze_avghallu.sh
+++ scripts/run_pipeline_rgbspec_san19pairfreeze_avghallu.sh
@@ -2,7 +2,7 @@
 PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         'configs/model_cfgs/pipeline_rgbspec_san19pairfreeze_avghallu.yaml' \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens.yaml' \
-    --train_cfg         'configs/train_cfgs/train_san_freeze.yaml' \
+    --train_cfg         'configs/train_cfgs/train_san_freeze_50.yaml' \
     --experiment_suffix 'san19pairfreeze_avghallu' \
     --is_training       true \
     --train_mode        'from_scratch' \
