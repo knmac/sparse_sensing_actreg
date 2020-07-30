189fc03aabbf3c543fec87688b51d2b77f0b19ca
knmac <khoinguyenx@gmail.com>
2020-07-29 21:31:25 -0500

    use adam

--- scripts/run_pipeline_rgbspec_san19pairfreeze_gruconvhallu.sh
+++ scripts/run_pipeline_rgbspec_san19pairfreeze_gruconvhallu.sh
@@ -2,7 +2,7 @@
 PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         'configs/model_cfgs/pipeline_rgbspec_san19pairfreeze_gruconvhallu.yaml' \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
-    --train_cfg         'configs/train_cfgs/train_san_freeze_50.yaml' \
+    --train_cfg         'configs/train_cfgs/train_san_freeze_adam_50.yaml' \
     --experiment_suffix 'san19pairfreeze_gruconvhallu' \
     --is_training       true \
     --train_mode        'from_scratch' \
