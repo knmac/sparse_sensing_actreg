e747f18c13e8bb064186e745932b43021aaaa1c7
knmac <khoinguyenx@gmail.com>
2020-09-22 12:40:03 -0500

    add python fault handler flag

--- scripts/run_pipeline_san19_rgbspec_112.sh
+++ scripts/run_pipeline_san19_rgbspec_112.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python main.py \
+PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg   'configs/model_cfgs/pipeline_simple_san19pair_rgbspec_112.yaml' \
     --dataset_cfg 'configs/dataset_cfgs/epickitchens.yaml' \
     --train_cfg   'configs/train_cfgs/train_tbn.yaml' \
