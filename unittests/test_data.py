7427cfde80a4740b227846d78fb3705e87accd9f
knmac <khoinguyenx@gmail.com>
2020-09-28 13:46:49 -0500

    update data unit test without flow

--- unittests/test_data.py
+++ unittests/test_data.py
@@ -17,20 +17,79 @@ from src.utils.misc import MiscUtils
 class TestData(unittest.TestCase):
     """Test dataset loading"""
 
-    def test_epic_kitchens(self):
-        """Test epic kitchens dataset"""
+    # def test_epic_kitchens(self):
+    #     """Test epic kitchens dataset"""
+    #     dataset_cfg = 'configs/dataset_cfgs/epickitchens.yaml'
+    #     dataset_name, dataset_params = ConfigLoader.load_dataset_cfg(dataset_cfg)
+    #     dataset_factory = DatasetFactory()
+
+    #     # Prepare some extra parameters
+    #     modality = ['RGB', 'Flow', 'Spec']
+    #     num_segments = 3
+    #     input_mean = {'RGB': [104, 117, 128], 'Flow': [128]}
+    #     input_std = {'RGB': [1], 'Flow': [1], 'Spec': [1]}
+    #     scale_size = {'RGB': 256, 'Flow': 256, 'Spec': 256}
+    #     crop_size = {'RGB': 224, 'Flow': 224, 'Spec': 224}
+    #     new_length = {'RGB': 1, 'Flow': 5, 'Spec': 1}
+
+    #     # Get augmentation and transforms
+    #     train_augmentation = MiscUtils.get_train_augmentation(modality, crop_size)
+    #     train_transform, val_transform = MiscUtils.get_train_val_transforms(
+    #         modality=modality,
+    #         input_mean=input_mean,
+    #         input_std=input_std,
+    #         scale_size=scale_size,
+    #         crop_size=crop_size,
+    #         train_augmentation=train_augmentation,
+    #     )
+
+    #     # Create dataset
+    #     dataset = dataset_factory.generate(
+    #         dataset_name, mode='val', modality=modality,
+    #         num_segments=num_segments, new_length=new_length,
+    #         transform=val_transform, **dataset_params,
+    #     )
+
+    #     assert dataset.name == 'epic_kitchens'
+
+    #     sample, label = dataset[0]
+    #     # Check shape
+    #     assert label['verb'] == 2
+    #     assert label['noun'] == 10
+    #     assert sample['RGB'].numpy().shape == (9, 224, 224)
+    #     assert sample['Flow'].numpy().shape == (30, 224, 224)
+    #     assert sample['Spec'].numpy().shape == (3, 256, 256)
+
+    #     # Check sum
+    #     assert (sample['RGB'].numpy().sum() - -15417859.0) < 0.1
+    #     assert (sample['Flow'].numpy().sum() - 12470523.0) < 0.1
+    #     assert (sample['Spec'].numpy().sum() - -1589548.8) < 0.1
+
+    #     # Check with data loader
+    #     data_loader = torch.utils.data.DataLoader(
+    #         dataset, batch_size=16, shuffle=False,
+    #         num_workers=4, pin_memory=True)
+
+    #     print('')
+    #     for i, (samples, labels) in enumerate(data_loader):
+    #         print(i, samples['RGB'].shape, samples['Flow'].shape, samples['Spec'].shape)
+    #         if i >= 2:
+    #             break
+
+    def test_epic_kitchens_rgbspec(self):
+        """Test epic kitchens dataset with only rgb and spectrogram"""
         dataset_cfg = 'configs/dataset_cfgs/epickitchens.yaml'
         dataset_name, dataset_params = ConfigLoader.load_dataset_cfg(dataset_cfg)
         dataset_factory = DatasetFactory()
 
         # Prepare some extra parameters
-        modality = ['RGB', 'Flow', 'Spec']
+        modality = ['RGB', 'Spec']
         num_segments = 3
-        input_mean = {'RGB': [104, 117, 128], 'Flow': [128]}
-        input_std = {'RGB': [1], 'Flow': [1], 'Spec': [1]}
-        scale_size = {'RGB': 256, 'Flow': 256, 'Spec': 256}
-        crop_size = {'RGB': 224, 'Flow': 224, 'Spec': 224}
-        new_length = {'RGB': 1, 'Flow': 5, 'Spec': 1}
+        input_mean = {'RGB': [104, 117, 128]}
+        input_std = {'RGB': [1], 'Spec': [1]}
+        scale_size = {'RGB': 256, 'Spec': 256}
+        crop_size = {'RGB': 224, 'Spec': 224}
+        new_length = {'RGB': 1, 'Spec': 1}
 
         # Get augmentation and transforms
         train_augmentation = MiscUtils.get_train_augmentation(modality, crop_size)
@@ -57,13 +116,7 @@ class TestData(unittest.TestCase):
         assert label['verb'] == 2
         assert label['noun'] == 10
         assert sample['RGB'].numpy().shape == (9, 224, 224)
-        assert sample['Flow'].numpy().shape == (30, 224, 224)
         assert sample['Spec'].numpy().shape == (3, 256, 256)
-
-        # Check sum
-        assert (sample['RGB'].numpy().sum() - -15417859.0) < 0.1
-        assert (sample['Flow'].numpy().sum() - 12470523.0) < 0.1
-        assert (sample['Spec'].numpy().sum() - -1589548.8) < 0.1
 
         # Check with data loader
         data_loader = torch.utils.data.DataLoader(
@@ -72,7 +125,7 @@ class TestData(unittest.TestCase):
 
         print('')
         for i, (samples, labels) in enumerate(data_loader):
-            print(i, samples['RGB'].shape, samples['Flow'].shape, samples['Spec'].shape)
+            print(i, samples['RGB'].shape, samples['Spec'].shape)
             if i >= 2:
                 break
 
