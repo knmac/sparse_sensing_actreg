4a32006d031d8a78b38264aa7ef99a9ea3bbb010
knmac <khoinguyenx@gmail.com>
2020-10-13 18:03:39 -0500

    add dgx launching scripts

---
+++ dgx/run_pipeline3_rgbspec_san19pairfreeze_halluconvlstm2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+echo "BALLISTA_OUTDIR = $BALLISTA_OUTDIR"
+
+BASEDIR=$(dirname "$0")
+echo "BASEDIR = $BASEDIR"
+cd "$BASEDIR/.."
+
+export CUPY_CACHE_DIR="$BASEDIR/../.cupy/kernel_cache"
+mkdir -p $CUPY_CACHE_DIR
+echo "CUPY_CACHE_DIR = $CUPY_CACHE_DIR"
+
+
+PYTHONFAULTHANDLER=1 python main.py \
+    --model_cfg         'configs/model_cfgs/pipeline3_rgbspec_san19pairfreeze_halluconvlstm2.yaml' \
+    --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
+    --train_cfg         'configs/train_cfgs/train_san_freeze_adam_50.yaml' \
+    --experiment_suffix 'san19pairfreeze_halluconvlstm2' \
+    --is_training       true \
+    --train_mode        'from_scratch' \
+    --best_metrics      'val_belief_loss' \
+    --best_fn           'min' \
+    --logdir            $BALLISTA_OUTDIR'/logs' \
+    --savedir           $BALLISTA_OUTDIR'/saved_models'
