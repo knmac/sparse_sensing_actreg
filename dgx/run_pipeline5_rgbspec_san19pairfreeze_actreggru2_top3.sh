18a90ff3e31290b4b42b3970934677fa48ebf0d3
knmac <khoinguyenx@gmail.com>
2020-10-16 16:18:48 -0500

    rename suffix

--- dgx/run_pipeline5_rgbspec_san19pairfreeze_actreggru2_top3.sh
+++ dgx/run_pipeline5_rgbspec_san19pairfreeze_actreggru2_top3.sh
@@ -14,7 +14,7 @@ PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         'configs/model_cfgs/pipeline5_rgbspec_san19pairfreeze_actreggru2_top3.yaml' \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
     --train_cfg         'configs/train_cfgs/train_san_freeze_adam_100.yaml' \
-    --experiment_suffix 'san19pairfreeze_actreggru2' \
+    --experiment_suffix 'san19pairfreeze_actreggru2_top3' \
     --is_training       true \
     --train_mode        'from_scratch' \
     --logdir            $BALLISTA_OUTDIR'/logs' \
