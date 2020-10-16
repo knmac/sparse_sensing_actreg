f351f190c0d79eae30b9fed46f2cc2fee620eb97
knmac <khoinguyenx@gmail.com>
2020-10-16 16:01:48 -0500

    add train 100 and change batch size

--- scripts/run_pipeline3_rgbspec_san19pairfreeze112@layer2-3_halluconvlstm2.sh
+++ scripts/run_pipeline3_rgbspec_san19pairfreeze112@layer2-3_halluconvlstm2.sh
@@ -2,7 +2,7 @@
 PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         'configs/model_cfgs/pipeline3_rgbspec_san19pairfreeze112@layer2-3_halluconvlstm2.yaml' \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
-    --train_cfg         'configs/train_cfgs/train_san_freeze_adam_50.yaml' \
+    --train_cfg         'configs/train_cfgs/train_san_freeze_adam_100.yaml' \
     --experiment_suffix 'san19pairfreeze112@layer2-3_halluconvlstm2' \
     --is_training       true \
     --train_mode        'from_scratch' \
