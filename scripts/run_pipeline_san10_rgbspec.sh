e00e8fc126a4f8e5185b9839134a28af79adf822
knmac <khoinguyenx@gmail.com>
2020-10-20 21:43:27 -0500

    add SAN10 and SAN15 scripts to train feature at 112x112 and 224x224 resolution

--- scripts/run_pipeline_san10_rgbspec.sh
+++ scripts/run_pipeline_san10_rgbspec.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 PYTHONFAULTHANDLER=1 python main.py \
-    --model_cfg   'configs/model_cfgs/pipeline_simple_san10pair_rgbspec_112.yaml' \
+    --model_cfg   'configs/model_cfgs/pipeline_simple_san10pair_rgbspec.yaml' \
     --dataset_cfg 'configs/dataset_cfgs/epickitchens.yaml' \
     --train_cfg   'configs/train_cfgs/train_san.yaml' \
-    --experiment_suffix 'san10pair_112_avg' \
+    --experiment_suffix 'san10pair_224_avg' \
     --is_training true \
     --train_mode  'from_scratch' \
     --logdir      'logs' \
