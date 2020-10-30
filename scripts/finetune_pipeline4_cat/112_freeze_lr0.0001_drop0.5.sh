1164073680e2ae4854ac2def395340c478cb6740
knmac <khoinguyenx@gmail.com>
2020-10-30 18:08:27 -0500

    rename

--- scripts/finetune_pipeline4_cat/112_freeze_lr0.0001_drop0.5.sh
+++ scripts/finetune_pipeline4_cat/112_freeze_lr0.0001_drop0.5.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+FREEZE="_freeze"
 LR="0.0001"
-DROP="0.25"
+DROP="0.5"
 
 echo "========================================================================"
+echo "FREEZE=${FREEZE}"
 echo "LR=${LR}"
 echo "DROP=${DROP}"
 echo "========================================================================"
@@ -10,8 +12,8 @@ echo "========================================================================"
 PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         "scripts/finetune_pipeline4_cat/configs_finetune/pipeline4_112_drop${DROP}.yaml" \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
-    --train_cfg         "scripts/finetune_pipeline4_cat/configs_finetune/train_lr${LR}.yaml" \
-    --experiment_suffix "finetune112_lr${LR}_drop${DROP}" \
+    --train_cfg         "scripts/finetune_pipeline4_cat/configs_finetune/train${FREEZE}_lr${LR}.yaml" \
+    --experiment_suffix "finetune112${FREEZE}_lr${LR}_drop${DROP}" \
     --is_training       true \
     --train_mode        'from_scratch' \
     --logdir            'logs' \
