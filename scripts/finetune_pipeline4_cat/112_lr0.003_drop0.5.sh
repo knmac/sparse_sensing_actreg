f09d81b394a530b2721243b5acdce4cf33e923b2
knmac <khoinguyenx@gmail.com>
2020-10-30 19:13:25 -0500

    add finetune scripts without dgx

---
+++ scripts/finetune_pipeline4_cat/112_lr0.003_drop0.5.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+FREEZE=""
+LR="0.003"
+DROP="0.5"
+
+echo "========================================================================"
+echo "FREEZE=${FREEZE}"
+echo "LR=${LR}"
+echo "DROP=${DROP}"
+echo "========================================================================"
+
+PYTHONFAULTHANDLER=1 python main.py \
+    --model_cfg         "scripts/finetune_pipeline4_cat/configs_finetune/pipeline4_112_drop${DROP}.yaml" \
+    --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
+    --train_cfg         "scripts/finetune_pipeline4_cat/configs_finetune/train${FREEZE}_lr${LR}.yaml" \
+    --experiment_suffix "finetune112${FREEZE}_lr${LR}_drop${DROP}" \
+    --is_training       true \
+    --train_mode        'from_scratch' \
+    --logdir            'logs' \
+    --savedir           'saved_models'
