8947027185fde34ee062da2f4aede1314e189a1e
knmac <khoinguyenx@gmail.com>
2021-01-07 20:26:52 -0600

    finetune san19

---
+++ scripts/finetune_san19/224_lr0.003.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PYTHONFAULTHANDLER=1 python main.py \
+    --model_cfg   'configs/model_cfgs/pipeline_simple_san19pair_rgbspec.yaml' \
+    --dataset_cfg 'configs/dataset_cfgs/epickitchens.yaml' \
+    --train_cfg   'scripts/finetune_san19/configs/train_san_lr0.003.yaml' \
+    --experiment_suffix 'san19pair_224_avg_finetune_lr0.003' \
+    --is_training true \
+    --train_mode  'from_scratch' \
+    --logdir      'logs' \
+    --savedir     'saved_models'
