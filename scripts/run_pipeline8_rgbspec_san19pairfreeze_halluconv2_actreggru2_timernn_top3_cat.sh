47431c80bbb4cc2f83d7f2207ecb2222e606f5df
knmac <khoinguyenx@gmail.com>
2021-03-04 11:07:23 -0600

    update pipeline8 with new finetuned pretraining

--- scripts/run_pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top3_cat.sh
+++ scripts/run_pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top3_cat.sh
@@ -2,7 +2,7 @@
 PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         'configs/model_cfgs/pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top3_cat.yaml' \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
-    --train_cfg         'configs/train_cfgs/train_san_freeze_adam_100.yaml' \
+    --train_cfg         'configs/train_cfgs/train_san_freeze_adam_50.yaml' \
     --experiment_suffix 'san19pairfreeze_halluconv2_actreggru2_timernn_top3_cat' \
     --is_training       true \
     --train_mode        'from_scratch' \
