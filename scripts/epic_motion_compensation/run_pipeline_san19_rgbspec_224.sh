378d1a2265270a4542af7af2b9293d555189c267
knmac <khoinguyenx@gmail.com>
2021-03-13 08:34:08 +0000

    train from pretrained

--- scripts/epic_motion_compensation/run_pipeline_san19_rgbspec_224.sh
+++ scripts/epic_motion_compensation/run_pipeline_san19_rgbspec_224.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 PYTHONFAULTHANDLER=1 python main.py \
-    --model_cfg   'configs/model_cfgs/pipeline_simple_san19pair_rgbspec.yaml' \
+    --model_cfg   'configs/model_cfgs/pipeline_simple_san19pair_rgbspec_epicpretrained.yaml' \
     --dataset_cfg 'configs/dataset_cfgs/epickitchens_compensation.yaml' \
-    --train_cfg   'configs/train_cfgs/train_san.yaml' \
+    --train_cfg   'configs/train_cfgs/train_san_50.yaml' \
     --experiment_suffix 'san19pair_224_avg' \
     --is_training true \
     --train_mode  'from_scratch' \
