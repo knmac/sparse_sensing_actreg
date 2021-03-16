e92ebc683c20a1ada9da9953bc2e654efa22bff5
knmac <khoinguyenx@gmail.com>
2021-03-15 21:51:42 -0500

    top0 does not have multihead

--- scripts/epic_motion_compensation/run_pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.sh
+++ scripts/epic_motion_compensation/run_pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PYTHONFAULTHANDLER=1 python main.py \
-    --model_cfg         'configs/model_cfgs/pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru3_timernn_top0_cat.yaml' \
+    --model_cfg         'configs/model_cfgs/pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.yaml' \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens_compensation.yaml' \
     --train_cfg         'configs/train_cfgs/train_san_freeze_adam_50.yaml' \
     --experiment_suffix 'san19pairfreeze_halluconv2_actreggru3_timernn_top0_cat_compensation' \
