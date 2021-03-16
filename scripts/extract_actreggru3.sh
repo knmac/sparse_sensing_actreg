0b50953554ff6c6c9951864f894ebb7424eea2da
knmac <khoinguyenx@gmail.com>
2021-03-15 23:25:00 -0500

    add scripts to run pipeline8 with multihead rnns

---
+++ scripts/extract_actreggru3.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Extract weight from action recognition weight with multihead (actreggru3)
+
+echo "Top1"
+python tools/extract_actreg_from_pipeline.py \
+    --weight        "/home/knmac/Dropbox/SparseSensing/DGX_training_logs/multihead/run_pipeline5_rgbspec_san19pairfreeze_actreggru3_top1_cat/best.model" \
+    --output_dir    "pretrained/finetuned/actreggru3_top1_cat__nofc1_1024hid_2lay"
+
+echo "Top2"
+python tools/extract_actreg_from_pipeline.py \
+    --weight        "/home/knmac/Dropbox/SparseSensing/DGX_training_logs/multihead/run_pipeline5_rgbspec_san19pairfreeze_actreggru3_top2_cat/best.model" \
+    --output_dir    "pretrained/finetuned/actreggru3_top2_cat__nofc1_1024hid_2lay"
+
+echo "Top3"
+python tools/extract_actreg_from_pipeline.py \
+    --weight        "/home/knmac/Dropbox/SparseSensing/DGX_training_logs/multihead/run_pipeline5_rgbspec_san19pairfreeze_actreggru3_top3_cat/best.model" \
+    --output_dir    "pretrained/finetuned/actreggru3_top3_cat__nofc1_1024hid_2lay"
