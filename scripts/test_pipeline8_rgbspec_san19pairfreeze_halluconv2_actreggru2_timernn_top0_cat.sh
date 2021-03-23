2757bf67f2d02e979f15963c6cbd60b6c49108dc
knmac <khoinguyenx@gmail.com>
2021-03-23 20:09:28 +0000

    change to test without gt

--- scripts/test_pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.sh
+++ scripts/test_pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         'configs/model_cfgs/test_pipeline8_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.yaml' \
-    --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
+    --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle_full.yaml' \
     --train_cfg         'configs/train_cfgs/test_pipeline8.yaml' \
     --experiment_suffix 'san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat' \
     --is_training       false \
