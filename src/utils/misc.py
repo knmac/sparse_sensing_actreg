05c4c47f3609ca77b506ae75366375973832f536
knmac <khoinguyenx@gmail.com>
2021-04-13 22:22:09 -0500

    add gradient plot tool

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -9,6 +9,8 @@ import torch
 import torchvision
 from torch.utils.data.dataloader import default_collate
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', '..')))
@@ -465,3 +467,30 @@ class MiscUtils:
         matched_1 = np.array([pts_1[k] for k in common_ids], dtype=np.float32)
         matched_2 = np.array([pts_2[k] for k in common_ids], dtype=np.float32)
         return matched_1, matched_2
+
+    @staticmethod
+    def plot_grad_flow(named_parameters):
+        ave_grads = []
+        max_grads = []
+        layers = []
+        for n, p in named_parameters:
+            if(p.requires_grad) and ("bias" not in n):
+                layers.append(n)
+                ave_grads.append(p.grad.abs().mean())
+                max_grads.append(p.grad.abs().max())
+
+        plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.3, lw=1, color="c")
+        plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.3, lw=1, color="b")
+        plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k")
+        plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+        plt.xlim(left=-1, right=len(ave_grads))
+        # plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+        plt.xlabel("Layers")
+        plt.ylabel("average gradient")
+        plt.title("Gradient flow")
+        plt.grid(True)
+        plt.legend([Line2D([0], [0], color="c", lw=4),
+                    Line2D([0], [0], color="b", lw=4),
+                    Line2D([0], [0], color="k", lw=4)],
+                   ['max-gradient', 'mean-gradient', 'zero-gradient'])
+        plt.show()
