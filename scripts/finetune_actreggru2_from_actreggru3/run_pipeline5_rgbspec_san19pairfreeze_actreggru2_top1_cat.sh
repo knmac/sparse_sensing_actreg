78b5e73a812ebe503ce4fac3d9071f077fe44cc4
knmac <khoinguyenx@gmail.com>
2021-04-20 16:59:08 -0500

    finetune singlehead from multihead

---
+++ scripts/finetune_actreggru2_from_actreggru3/run_pipeline5_rgbspec_san19pairfreeze_actreggru2_top1_cat.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PYTHONFAULTHANDLER=1 python main.py \
+    --model_cfg         'scripts/finetune_actreggru2_from_actreggru3/configs/pipeline5_rgbspec_san19pairfreeze_actreggru2_top1_cat.yaml' \
+    --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle.yaml' \
+    --train_cfg         'configs/train_cfgs/train_san_freeze_adam_30_lr0.0003.yaml' \
+    --experiment_suffix 'san19pairfreeze_actreggru2_top1_cat__fromactreggru3' \
+    --is_training       true \
+    --train_mode        'from_scratch' \
+    --logdir            'logs' \
+    --savedir           'saved_models'
