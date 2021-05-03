b1c554437b9d6a9bb8d275398b82f3cd455f0613
knmac <khoinguyenx@gmail.com>
2021-05-03 17:11:55 -0500

    allow to split to multiple parts

--- tools/preprocess_rgbds.py
+++ tools/preprocess_rgbds.py
@@ -14,8 +14,11 @@ from src.utils.misc import MiscUtils
 
 
 class EpicWrapper(EpicKitchenDataset):
-    def __init__(self, **kwargs):
+    def __init__(self, n_parts, part_id, **kwargs):
         kwargs.update({'modality': ['RGBDS']})
+
+        self.n_parts = n_parts
+        self.part_id = part_id
         super(EpicWrapper, self).__init__(**kwargs)
 
     def __getitem__(self, index):
@@ -25,8 +28,16 @@ class EpicWrapper(EpicKitchenDataset):
             self._load_data('RGBDS', record, idx)
         print('vid {}/{}: DONE!'.format(index, len(self.video_list)))
 
+    def _parse_list(self):
+        """Parse from pandas data frame to list of EpicVideoRecord objects"""
+        super(EpicWrapper, self)._parse_list()
 
-def process(dataset_cfg, n_threads, mode):
+        # Cut down the list
+        self.video_list = [self.video_list[i] for i in range(len(self.video_list))
+                           if i % self.n_parts == self.part_id]
+
+
+def process(dataset_cfg, n_threads, n_parts, part_id, mode):
     print('='*30 + '\n' + mode + '\n' + '='*30)
     _, dataset_params = ConfigLoader.load_dataset_cfg(dataset_cfg)
 
@@ -36,7 +47,7 @@ def process(dataset_cfg, n_threads, mode):
         'modality': ['RGBDS'],
         'new_length': {'RGBDS': 1},
     })
-    dataset = EpicWrapper(**dataset_params)
+    dataset = EpicWrapper(n_parts, part_id, **dataset_params)
 
     loader_params = {
         'batch_size': n_threads,
@@ -55,15 +66,24 @@ def process(dataset_cfg, n_threads, mode):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--n_threads', type=int, default=48)
+    parser.add_argument('-n', '--n_threads', type=int, default=48,
+                        help='Number of workers and batch size')
+    parser.add_argument('-p', '--n_parts', type=int, default=14,
+                        help='Total number of parts to divide the list')
+    parser.add_argument('-i', '--part_id', type=int, default=0,
+                        help='Part index')
     parser.add_argument('-d', '--dataset_cfg', type=str,
-                        default='configs/dataset_cfgs/epickitchens_noshuffle_rgbds.yaml')
-                        # default='configs/dataset_cfgs/epickitchens_p01_08.yaml')
+                        default='configs/dataset_cfgs/epickitchens_noshuffle_rgbds.yaml',
+                        # default='configs/dataset_cfgs/epickitchens_p01_08.yaml',
+                        help='Dataset configuration')
     args = parser.parse_args()
 
-    process(args.dataset_cfg, args.n_threads, 'train')
-    process(args.dataset_cfg, args.n_threads, 'val')
-    process(args.dataset_cfg, args.n_threads, 'test')
+    assert 0 <= args.part_id < args.n_parts, \
+        'part_id must be in [0, n_parts)'
+    print('Part {} out of {}'.format(args.part_id+1, args.n_parts))
+    process(args.dataset_cfg, args.n_threads, args.n_parts, args.part_id, 'train')
+    process(args.dataset_cfg, args.n_threads, args.n_parts, args.part_id, 'val')
+    process(args.dataset_cfg, args.n_threads, args.n_parts, args.part_id, 'test')
     return 0
 
 
