85bf6f1f395d802a795dd33bdf025795002088c3
knmac <khoinguyenx@gmail.com>
2021-05-13 14:41:45 -0500

    fix typo

--- scripts/test_pipeline9_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.sh
+++ scripts/test_pipeline9_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.sh
@@ -3,7 +3,7 @@ PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg         'configs/model_cfgs/test_pipeline9_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat.yaml' \
     --dataset_cfg       'configs/dataset_cfgs/epickitchens_noshuffle_full.yaml' \
     --train_cfg         'configs/train_cfgs/test_pipeline9.yaml' \
-    --experiment_suffix 'san19pairfreeze_halluconv2_actreggru3_timernn_top1_cat' \
+    --experiment_suffix 'san19pairfreeze_halluconv2_actreggru3_timernn_top0_cat' \
     --is_training       false \
     --logdir            'logs' \
     --savedir           'saved_models'
