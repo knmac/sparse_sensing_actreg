1213a0d1371e1ed70aeb19dcdb9d054dfe18a395
knmac <khoinguyenx@gmail.com>
2021-05-17 15:59:46 -0500

    fintune pipeline9

---
+++ scripts/finetune_pipeline9/top0_2frame_lr0.0003.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+FRAME="2frame"
+LR="lr0.0003"
+
+PYTHONFAULTHANDLER=1 python main.py \
+    --model_cfg         "scripts/finetune_pipeline9/configs/pipeline9_rgbspec_san19pairfreeze_halluconv2_actreggru2_timernn_top0_cat_${FRAME}.yaml" \
+    --dataset_cfg       "configs/dataset_cfgs/epickitchens_noshuffle.yaml" \
+    --train_cfg         "scripts/finetune_pipeline9/configs/train_san_freeze_adam_30_${LR}.yaml" \
+    --experiment_suffix "finetune_pipeline9__${FRAME}_${LR}" \
+    --is_training       true \
+    --train_mode        "from_scratch" \
+    --logdir            "logs" \
+    --savedir           "saved_models"
