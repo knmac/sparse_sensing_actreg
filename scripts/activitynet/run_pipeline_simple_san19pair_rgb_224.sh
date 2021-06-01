658d514292e20a0d527b7825f3840798a9dae0f7
knmac <khoinguyenx@gmail.com>
2021-06-01 15:43:24 -0500

    change parameters to match arnet

--- scripts/activitynet/run_pipeline_simple_san19pair_rgb_224.sh
+++ scripts/activitynet/run_pipeline_simple_san19pair_rgb_224.sh
@@ -2,7 +2,8 @@
 PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg   'configs/model_cfgs/activitynet/pipeline_simple_san19pair_rgb_224.yaml' \
     --dataset_cfg 'configs/dataset_cfgs/activitynet.yaml' \
-    --train_cfg   'configs/train_cfgs/train_san.yaml' \
+    --train_cfg   'configs/train_cfgs/activitynet/train_san.yaml' \
+    --seed        1007 \
     --experiment_suffix 'san19pair_224' \
     --is_training true \
     --train_mode  'from_scratch' \
