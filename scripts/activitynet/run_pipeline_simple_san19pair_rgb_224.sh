e00b5fde3ba803fed7f8502df7e248594610e6e6
knmac <khoinguyenx@gmail.com>
2021-06-23 17:10:55 -0500

    increase number of epochs

--- scripts/activitynet/run_pipeline_simple_san19pair_rgb_224.sh
+++ scripts/activitynet/run_pipeline_simple_san19pair_rgb_224.sh
@@ -2,7 +2,7 @@
 PYTHONFAULTHANDLER=1 python main.py \
     --model_cfg   'configs/model_cfgs/activitynet/pipeline_simple_san19pair_rgb_224.yaml' \
     --dataset_cfg 'configs/dataset_cfgs/activitynet.yaml' \
-    --train_cfg   'configs/train_cfgs/activitynet/train_san.yaml' \
+    --train_cfg   'configs/train_cfgs/activitynet/train_san50.yaml' \
     --seed        1007 \
     --experiment_suffix 'san19pair_224' \
     --is_training true \
