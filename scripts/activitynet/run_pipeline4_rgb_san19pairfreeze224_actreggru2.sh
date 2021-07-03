2d0ab00a205fe3e4e52b2e5bc857738991bbf456
knmac <khoinguyenx@gmail.com>
2021-07-02 21:49:51 -0500

    add pipeline4 for activitynet

---
+++ scripts/activitynet/run_pipeline4_rgb_san19pairfreeze224_actreggru2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+PYTHONFAULTHANDLER=1 python main.py \
+    --model_cfg         'configs/model_cfgs/activitynet/pipeline4_rgb_san19pairfreeze224_actreggru2.yaml' \
+    --dataset_cfg       'configs/dataset_cfgs/activitynet.yaml' \
+    --train_cfg         'configs/train_cfgs/train_san_freeze_adam_50.yaml' \
+    --seed              1007 \
+    --experiment_suffix 'san19pairfreeze224_actreggru2' \
+    --is_training       true \
+    --train_mode        'from_scratch' \
+    --logdir            'logs' \
+    --savedir           'saved_models'
